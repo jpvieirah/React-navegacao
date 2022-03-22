c03b99e61a4aeba4272661b05b420ea8b3d50c84
helsin <jp201101@gmail.com>
2022-03-22 12:15:05 -0300

    Navegação com parâmetro

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -3,11 +3,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom"
 import About from '../../views/examples/About';
 import Home from '../../views/examples/Home';
+import Param from '../../views/examples/Param';
 
 const Content = props => (
     <main className="Content" >
             <Routes>
             <Route path="/about" element={<About />}>
+            </Route>
+            <Route path="/param/:id" element={<Param />}>
             </Route>
             <Route path="/" element={<Home />}>
             </Route>
